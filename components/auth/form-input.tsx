2bec0ebe5cee19f5a2c09bb4f2e08000404e27b7
Aayush <ayushkarkee6@gmail.com>
2024-12-06 12:53:18 +0545

    formatting changed

--- components/auth/form-input.tsx
+++ components/auth/form-input.tsx
@@ -1,14 +1,21 @@
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { Control, FieldValues, Path } from "react-hook-form";
 
-type FormInputProps<T extends FieldValues> = React.ComponentPropsWithRef<"input"> & {
-  control: Control<T>;
-  name: Path<T>;
-  label: string;
-  isPending?: boolean;
-};
+type FormInputProps<T extends FieldValues> =
+  React.ComponentPropsWithRef<"input"> & {
+    control: Control<T>;
+    name: Path<T>;
+    label: string;
+    isPending?: boolean;
+  };
 
 export const FormInput = <T extends FieldValues>(props: FormInputProps<T>) => {
   const { control, name, label, isPending, disabled, ...rest } = props;
