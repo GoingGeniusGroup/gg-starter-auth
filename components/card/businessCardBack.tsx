caddef7f3b65f1b86e0a15459a51fb0a79aa724b
Aayush <ayushkarkee6@gmail.com>
2024-12-09 12:18:54 +0545

    creating businesscard front and back component separately

---
+++ components/card/businessCardBack.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BusinessCardBack = () => {
+  return <div>BusinessCardBack</div>;
+};
+
+export default BusinessCardBack;
---
+++ components/card/businessCardFront.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BusinessCardFront = () => {
+  return <div>BusinessCardFront</div>;
+};
+
+export default BusinessCardFront;
