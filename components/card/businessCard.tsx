e523d6964535d280d611216eb0cdc04da9d3e183
Aayush <ayushkarkee6@gmail.com>
2024-12-09 15:50:08 +0545

    flipping animation for businesscard

--- components/card/businessCard.tsx
+++ components/card/businessCard.tsx
@@ -1,15 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import BusinessCardFront from "./businessCardFront";
 import BusinessCardBack from "./businessCardBack";
+import "../../styles/globals.css";
 
 const BusinessCard = () => {
-    const [fliped, setFliped] = useState(false);
+  const [flipped, setFlipped] = useState(false);
+
+  const handleCardClick = () => {
+    setFlipped((prev) => !prev);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <BusinessCardFront />
-      <BusinessCardBack />
+      <motion.div
+        className="w-[336px] h-[192px]"
+        onClick={handleCardClick}
+        animate={{ rotateY: flipped ? 180 : 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        <motion.div
+          className="w-full h-full shadow-md"
+          transition={{ duration: 0.7 }}
+          animate={{ rotateY: flipped ? 180 : 0 }}
+        >
+          <motion.div
+            className="absolute backface-hidden"
+            transition={{ duration: 0.7 }}
+            animate={{ rotateY: flipped ? 180 : 0 }}
+          >
+            <BusinessCardFront />
+          </motion.div>
+          <motion.div
+            className="absolute backface-hidden"
+            initial={{ rotateY: 180 }}
+            transition={{ duration: 0.7 }}
+            animate={{ rotateY: flipped ? 0 : 180 }}
+          >
+            <BusinessCardBack />
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
