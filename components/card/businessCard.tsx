ee86abab10a0f08a3206bc7ebd84af60f83d7444
Aayush <ayushkarkee6@gmail.com>
2024-12-09 15:05:21 +0545

    wroking the flip style for businesscard

--- components/card/businessCard.tsx
+++ components/card/businessCard.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import BusinessCardFront from "./businessCardFront";
 import BusinessCardBack from "./businessCardBack";
 
 const BusinessCard = () => {
+    const [fliped, setFliped] = useState(false);
   return (
     <div className="flex justify-center items-center h-screen">
+      <BusinessCardFront />
       <BusinessCardBack />
     </div>
   );
