80829adf9b8f1a8b27a71a27b613132c76892df2
Aayush <ayushkarkee6@gmail.com>
2024-12-09 14:36:53 +0545

    working in businesscardback

--- components/card/businessCard.tsx
+++ components/card/businessCard.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import BusinessCardFront from "./businessCardFront";
+import BusinessCardBack from "./businessCardBack";
 
 const BusinessCard = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <BusinessCardFront />
+      <BusinessCardBack />
     </div>
   );
 };
--- components/card/businessCardBack.tsx
+++ components/card/businessCardBack.tsx
@@ -1,4 +1,14 @@
 import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { CiMail, CiLocationOn } from "react-icons/ci";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import Image from "next/image";
 
 const BusinessCardBack = () => {
   return <div>BusinessCardBack</div>;
