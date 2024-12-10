9c3fbfbac1830e8109d5204820a05ec1db71184b
Aayush <ayushkarkee6@gmail.com>
2024-12-10 23:31:55 +0545

    Studentcard component with flip animation completed

--- components/card/studentCard.tsx
+++ components/card/studentCard.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import "../../styles/globals.css";
 import StudentCardFront from "./studentCardFront";
 import StudentCardBack from "./studentCardBack";
 
 const StudentCard = () => {
+  const [flipped, setFlipped] = useState(false);
+
+  const handleCardClick = () => {
+    setFlipped((prev) => !prev);
+  };
+
   return (
-    <div>
-      <StudentCardBack />
+    <div className="">
+      <motion.div
+        className="w-[266px] h-[413px]"
+        onClick={handleCardClick}
+        animate={{ rotateY: flipped ? 180 : 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        <motion.div
+          className="w-full h-full shadow-md"
+          transition={{ duration: 0.7 }}
+          animate={{ rotateY: flipped ? 180 : 0 }}
+        >
+          <motion.div
+            className="absolute backface-hidden"
+            transition={{ duration: 0.7 }}
+            animate={{ rotateY: flipped ? 180 : 0 }}
+          >
+            <StudentCardFront />
+          </motion.div>
+          <motion.div
+            className="absolute backface-hidden"
+            initial={{ rotateY: 180 }}
+            transition={{ duration: 0.7 }}
+            animate={{ rotateY: flipped ? 0 : 180 }}
+          >
+            <StudentCardBack />
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
