a0c52269674934db7a087d109e2e60a4c4f3da92
Prija <shresthaprija123@gmail.com>
2024-12-11 19:09:28 +0545

    dashboard section

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -12,6 +12,7 @@ export default async function Home() {
     <>
       <div>Hello {user?.name}</div>
       {/* <VideoHome /> */}
+      
     </>
   )
 }
---
+++ app/dashboard/product/page.tsx
@@ -0,0 +1,10 @@
+const ProductPage = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-xl font-bold">Product Page</h1>
+      <p>Welcome to the Product section of the dashboard!</p>
+    </div>
+  );
+};
+
+export default ProductPage;
