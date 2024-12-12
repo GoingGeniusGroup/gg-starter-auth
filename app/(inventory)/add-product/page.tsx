41130809ac41d406aca840d6cb2f13d0bd344a30
rajarya199 <aryalrajan078@gmail.com>
2024-12-13 00:49:31 +0545

    product form-i

---
+++ app/(inventory)/add-product/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductForm from '@/components/inventoryComponent/ProductForm' 
+
+const page = () => {
+  return (
+    <div>
+      <ProductForm/>
+
+    </div>
+  )
+}
+
+export default page
