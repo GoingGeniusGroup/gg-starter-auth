566d28b6a614f4429f0e66400b0a4a7c651e5f21
rajarya199 <aryalrajan078@gmail.com>
2024-12-13 12:53:07 +0545

    supplier modify-ii

--- app/(inventory)/inventory/supplier/page.tsx
+++ app/(inventory)/inventory/supplier/page.tsx
@@ -45,7 +45,7 @@ const Supplier = () => {
 
       
       {showForm && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+        <div className="fixed inset-0 backdrop-blur-sm flex justify-center items-center z-50">
           <div className="rounded-lg shadow-lg relative w-full max-w-lg">
             <SupplierForm onCancel={hideForm} />
           </div>
