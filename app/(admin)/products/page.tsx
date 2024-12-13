7dca1edc6298e6c7046a2faebb1a19afcd1c8161
gg-ayush <gg.ayushtamang@gmail.com>
2024-12-13 16:18:30 +0545

    product_page_commented_for_deployment

--- app/(admin)/products/page.tsx
+++ app/(admin)/products/page.tsx
@@ -1,26 +1,26 @@
 
 
-import { Button } from "@/components/ui/button";
-// import PageHeader from "@/components/PageHeader/PageHeader";
-import Link from "next/link";
-import ProductTable from "./_components/ProductTable";
-// import ProductForm from "@/components/form/ProductForm";
-// import ProductModel from "@/components/Model/ProductModel";
+// import { Button } from "@/components/ui/button";
+// // import PageHeader from "@/components/PageHeader/PageHeader";
+// import Link from "next/link";
+// import ProductTable from "./_components/ProductTable";
+// // import ProductForm from "@/components/form/ProductForm";
+// // import ProductModel from "@/components/Model/ProductModel";
 
-const page = () => {
-  return (
-    <>
-      <div className="container mx-auto flex justify-between items-center gap-4">
-        All Products
+// const page = () => {
+//   return (
+//     <>
+//       <div className="container mx-auto flex justify-between items-center gap-4">
+//         All Products
         
-        <Button asChild variant={"secondary"}>
-          <Link href="/admin/products/new">Add Product</Link>
-        </Button>
-      </div>
-      <ProductTable />
-{/*       <ProductModel/> */}
-    </>
-  );
-};
+//         <Button asChild variant={"secondary"}>
+//           <Link href="/admin/products/new">Add Product</Link>
+//         </Button>
+//       </div>
+//       <ProductTable />
+// {/*       <ProductModel/> */}
+//     </>
+//   );
+// };
 
-export default page;
+// export default page;
