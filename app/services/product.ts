6756ead75c2dc0a3c66252f08e85011f83edd747
Aayush <ayushkarkee6@gmail.com>
2024-12-13 10:37:04 +0545

    update :: cards modal schema

--- app/services/product.ts
+++ app/services/product.ts
@@ -1,33 +1,30 @@
 import { db } from "@/lib/db";
 
-
 export const getProducts = async () => {
   try {
     const products = await db.product.findMany({
       select: {
         id: true,
         name: true,
-       
+
         description: true,
         quantityInStock: true,
-      
+
         salePrice: true,
         costPrice: true,
         status: true,
-        category:{
-            select:{
-                categoryName:true
-            }
-        }
-       
+        category: {
+          select: {
+            categoryName: true,
+          },
+        },
       },
-     
     });
 
-    return products
+    return products;
   } catch (error) {
     console.log(error);
-    
-    return null
+
+    return null;
   }
 };
