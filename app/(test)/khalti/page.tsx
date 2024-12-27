08e8104ef1fa90d3848c3c4a149c52ddf90d873b
Aayush <ayushkarkee6@gmail.com>
2024-12-27 15:24:45 +0545

    update :: created test folder to test khalti component

---
+++ app/(test)/khalti/page.tsx
@@ -0,0 +1,12 @@
+import KhaltiPayment from "@/app/components/paymentgateway/khalti";
+import React from "react";
+
+const Khalti = () => {
+  return (
+    <div>
+      <KhaltiPayment />
+    </div>
+  );
+};
+
+export default Khalti;
