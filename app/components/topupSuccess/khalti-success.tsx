6148b54a6d21cf0f19d5754ce7aea2c11bcef6e3
Aayush <ayushkarkee6@gmail.com>
2025-01-09 13:33:53 +0545

    update :: created khalti success  component succesfully

--- app/components/topupSuccess/khalti-success.tsx
+++ app/components/topupSuccess/khalti-success.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CheckCircle } from "lucide-react";
 import { Button } from "../ui/button";
 import {
   Card,
@@ -21,5 +22,41 @@ interface khaltiSuccessProps {
 }
 
 export function KhaltiSuccess({ topup }: khaltiSuccessProps) {
-  return <Card className="w-full max-w-md"></Card>;
+  return (
+    <Card className="w-full max-w-md relative">
+      <CardHeader>
+        <CardTitle className="flex">
+          <CheckCircle className="text-green-500 mr-2" /> Payment Successful
+        </CardTitle>
+        <CardDescription>
+          Your Khalti Payment has been processed successfully
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <dl className="grid grid-cols-2 gap-4">
+          <div>
+            <dt className="font-medium text-gray-500">{topup.amount}</dt>
+            <dd className="mt-1">NPR 1000</dd>
+          </div>
+          <div>
+            <dt className="font-medium text-gray-500">{topup.topupStatus}</dt>
+            <dd className="mt-1">Success</dd>
+          </div>
+          <div>
+            <dt className="font-medium text-gray-500">Date</dt>
+            <dd className="mt-1">{topup.createdAt.toLocaleString()}</dd>
+          </div>
+          <div>
+            <dt className="font-medium text-gray-500">Type</dt>
+            <dd className="mt-1">{topup.topupType}</dd>
+          </div>
+        </dl>
+        <Link href={"/"}>
+          <Button variant={"outline"} className="w-full mt-6">
+            Go to home
+          </Button>
+        </Link>
+      </CardContent>
+    </Card>
+  );
 }
