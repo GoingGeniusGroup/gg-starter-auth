631a15848913f3a12c7e793025931310895481ed
Aayush <ayushkarkee6@gmail.com>
2025-01-09 11:50:48 +0545

    merged with main

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,25 +50,25 @@ export default async function RootLayout({
                   enableSystem
                 >
                   {/* <AuroraBackground> */}
-                    <UserProvider>
-                      {/* Theme switcher */}
-                      {/* <div className="absolute top-[8px] right-[73px] z-50">
+                  <UserProvider>
+                    {/* Theme switcher */}
+                    {/* <div className="absolute top-[8px] right-[73px] z-50">
                         <ThemeSwitcher />
                       </div> */}
 
-                      {/* Dock section */}
-                      {/* <div className="w-full z-40">
+                    {/* Dock section */}
+                    {/* <div className="w-full z-40">
                         <div className="p-4 text-black dark:text-white">
                           <FloatingDockInvertedComponent />
                         </div>
                       </div> */}
 
-                      {/* Content section */}
-                      <div className="flex-1 px-8 py-4 w-full overflow-auto">
-                        {children}
-                      </div>
-                    </UserProvider>
-                    <ToastProvider />
+                    {/* Content section */}
+                    <div className="flex-1 px-8 py-4 w-full overflow-auto">
+                      {children}
+                    </div>
+                  </UserProvider>
+                  <ToastProvider />
                   {/* </AuroraBackground> */}
                 </ThemeProvider>
               </MobileSimulatorProvider>
@@ -93,25 +93,25 @@ export default async function RootLayout({
           <MobileSimulatorProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
               {/* <AuroraBackground> */}
-                <UserProvider>
-                  {/* Theme switcher */}
-                  {/* <div className="absolute top-[8px] right-[73px] z-50">
+              <UserProvider>
+                {/* Theme switcher */}
+                {/* <div className="absolute top-[8px] right-[73px] z-50">
                     <ThemeSwitcher />
                   </div> */}
 
-                  {/* Dock section */}
-                  <div className="w-full z-40">
-                    {/* <div className="p-4 text-black dark:text-white">
+                {/* Dock section */}
+                <div className="w-full z-40">
+                  {/* <div className="p-4 text-black dark:text-white">
                       <FloatingDockInvertedComponent />
                     </div> */}
-                  </div>
+                </div>
 
-                  {/* Content section */}
-                  <div className="flex-1 px-8 py-4 w-full overflow-auto">
-                    {children}
-                  </div>
-                </UserProvider>
-                <ToastProvider />
+                {/* Content section */}
+                <div className="flex-1 px-8 py-4 w-full overflow-auto">
+                  {children}
+                </div>
+              </UserProvider>
+              <ToastProvider />
               {/* </AuroraBackground> */}
             </ThemeProvider>
           </MobileSimulatorProvider>
