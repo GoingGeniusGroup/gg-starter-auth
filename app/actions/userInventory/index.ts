f505f62f62e03ac66c8faebde390237974fb7665
Avashbhandari01 <avash115@gmail.com>
2025-01-13 17:38:48 +0545

    Get user inventory by user id.

--- app/actions/userInventory/index.ts
+++ app/actions/userInventory/index.ts
@@ -1,11 +1,22 @@
 "use server";
 import { cache } from "@/lib/cache";
 import { db } from "@/lib/db";
+import { currentUser } from "@/lib/auth";
 
 export const getInventory = cache(
   async () => {
     try {
+      const user = await currentUser(); 
+      if (!user || !user.id) {
+        throw new Error("User not authenticated");
+      }
+
+      const userId = user.id; 
+
       const inventory = await db.userInventory.findMany({
+        where: {
+          userId, 
+        },
         include: {
           VirtualProduct: {
             select: {
@@ -16,6 +27,7 @@ export const getInventory = cache(
           },
         },
       });
+
       return inventory;
     } catch (error) {
       console.error("Error fetching user inventory:", error);
