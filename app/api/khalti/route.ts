5f2a4e293e0244abba5550e906fd7229741eaa68
Aayush <ayushkarkee6@gmail.com>
2025-01-16 22:08:36 +0545

    update :: api of khalti payment

--- app/api/khalti/route.ts
+++ app/api/khalti/route.ts
@@ -1,33 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
-// export async function POST(req: NextRequest) {
-//   console.log(req);
-//   const payload = req.json();
-//   const khaltiResponse = await axios.post(
-//     "https://dev.khalti.com/api/v2/epayment/initiate/",
-//     payload,
-//     {
-//       headers: {
-//         Authorization: `key ${process.env.NEXT_PUBLIC_KHALTI_SECRET_KEY}`,
-//         "Content-Type": "application/json",
-//       },
-//     }
-//   );
+export async function POST(req: NextRequest) {
+  try {
+    const payload = await req.json();
+    const khaltiResponse = await axios.post(
+      "https://dev.khalti.com/api/v2/epayment/initiate/",
+      payload,
+      {
+        headers: {
+          Authorization: `key ${process.env.NEXT_PUBLIC_KHALTI_SECRET_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-//   if (khaltiResponse) {
-//     return NextResponse.json({
-//       success: true,
-//       data: khaltiResponse?.data,
-//     });
-//   } else {
-//     return NextResponse.json({
-//       success: false,
-//       message: "Something went wrong",
-//     });
-//   }
-// }
-
-export async function GET() {
-  return NextResponse.json({ message: "success" });
+    if (khaltiResponse) {
+      return NextResponse.json({
+        success: true,
+        data: khaltiResponse?.data,
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Something went wrong",
+      });
+    }
+  } catch (error) {
+    console.log(`khalti error: ${error}`);
+    throw new Error("Something went wrong");
+  }
 }
