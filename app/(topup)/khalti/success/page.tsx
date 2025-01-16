82db6cbcd0f7032851d3bd0a6e751b039b45ea38
Aayush <ayushkarkee6@gmail.com>
2025-01-16 23:01:35 +0545

    update :: changes in khalti success page

--- app/(topup)/khalti/success/page.tsx
+++ app/(topup)/khalti/success/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const KhaltiSuccessPage = async () => {
   return (
-    <div>
+    <div className="flex items-center justify-center mt-20">
       <KhaltiSuccess />
     </div>
   );
