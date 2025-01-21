2ea3618d69f1319220fc735350cb13a3a463ef77
Prija <shresthaprija123@gmail.com>
2025-01-21 14:57:07 +0545

    khalti integrated in wallet component

--- app/components/MobileSimulator/provider/MobileSimulatorContext.tsx
+++ app/components/MobileSimulator/provider/MobileSimulatorContext.tsx
@@ -15,8 +15,11 @@ import {
   FaBell,
   FaShoppingCart,
   FaSignInAlt,
+  FaBriefcase,
+  FaUserCircle,
   FaUserPlus,
 } from "react-icons/fa";
+
 import { MdInventory } from "react-icons/md";
 import ShopSection from "../../shop/ShopSection";
 import { LoginForm } from "@/components/form/login-form";
@@ -28,6 +31,8 @@ import SimulatorToggleButton from "../SimulatorToggleButton";
 import NotificationComponent from "@/components/Notification/NotificationComponent";
 import ShopComponent from "../../ShopComponent/ShopComponent";
 import UserInventoryComponent from "../../UserInventory/UserInventoryComponent";
+import ProfileComponent from "@/components/profile/subComponents/ProfileComponent";
+import WalletComponent from "@/components/profile/subComponents/WalletComponent";
 // import { getColorsbyUserId } from "@/services/color";
 // import { ThemeType } from "@prisma/client";
 
@@ -54,18 +59,21 @@ interface MobileSimulatorContextType {
 const backgrounds = [
   {
     name: "Cosmic Nebula",
-    class:
-      "bg-gradient-to-b from-indigo-600 to-purple-600 via-pink-500 text-white",
+    class: "bg-gradient-to-b from-indigo-600 to-white-600  text-white",
   },
+  // {
+  //   name: "Cyberpunk City",
+  //   class:
+  //     "bg-gradient-to-b from-blue-900 to-purple-800  text-white",
+  // },
   {
     name: "Cyberpunk City",
-    class:
-      "bg-gradient-to-b from-blue-900 to-purple-800 via-pink-700 text-white",
+    class: "bg-gradient-to-b from-yellow-300 to-white-800  text-white",
   },
   {
     name: "Glimmering Stars",
     class:
-      "bg-gradient-to-b from-blue-900 to-purple-800 via-pink-700 text-white",
+      "bg-gradient-to-b from-pink-900 to-purple-800 via-red-700 text-white",
   },
   {
     name: "Dark Matter",
@@ -210,6 +218,18 @@ export const MobileSimulatorProvider = ({
       },
       {
         id: 4,
+        title: "UserProfile",
+        icon: <FaUserCircle />,
+        content: <ProfileComponent />,
+      },
+      {
+        id: 5,
+        title: "Wallet",
+        icon: <FaBriefcase />,
+        content: <WalletComponent />,
+      },
+      {
+        id: 6,
         title: "Notifications",
         icon: <FaBell />,
         content: <NotificationComponent />,
