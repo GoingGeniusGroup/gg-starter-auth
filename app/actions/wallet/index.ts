5cbaf5bb81270d823afabdb23641b71a61c500e8
Aayush <ayushkarkee6@gmail.com>
2025-01-23 17:22:34 +0545

    update :: action to get balance

--- app/actions/wallet/index.ts
+++ app/actions/wallet/index.ts
@@ -1,4 +1,37 @@
-"use server"
+"use server";
 
+import { currentUser } from "@/app/lib/auth";
 import { cache } from "@/lib/cache";
 import { db } from "@/lib/db";
+
+export const getUserBalance = cache(
+  async () => {
+    try {
+      const user = await currentUser();
+
+      if (!user || !user.id) {
+        throw new Error("User doesn't exists");
+      }
+
+      const userId = user.id;
+
+      const userBalance = db.user.findUnique({
+        where: { id: userId },
+        select: { balance: true },
+      });
+
+      return {
+        success: true,
+        data: userBalance,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        success: false,
+        error: "Something went wrong",
+      };
+    }
+  },
+  ["getUserBalance"],
+  { revalidate: 60 }
+);
