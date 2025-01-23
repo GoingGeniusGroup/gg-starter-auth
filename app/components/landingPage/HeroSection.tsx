8fd9044f6d4b15b0c60b82d31e1a857e8134daa5
Aayush <ayushkarkee6@gmail.com>
2025-01-23 12:52:22 +0545

    update :: working in herosection

--- app/components/landingPage/HeroSection.tsx
+++ app/components/landingPage/HeroSection.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Card } from "../ui/card";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="col-span-2">
+        <Card></Card>
+      </div>
+      <div className="col-span-1">right</div>
+    </div>
+  );
 };
 
 export default HeroSection;
