ca9c53d5160c6f6b0dfe2d5a2eee317f6faad4ff
Aayush <ayushkarkee6@gmail.com>
2025-01-23 13:19:23 +0545

    update :: formatted the styles and layout for hero section

--- app/components/landingPage/HeroSection.tsx
+++ app/components/landingPage/HeroSection.tsx
@@ -1,13 +1,41 @@
 import React from "react";
-import { Card } from "../ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3">
-      <div className="col-span-2">
-        <Card></Card>
+    <div className="flex flex-col lg:flex-row items-center justify-between gap-12 mb-24">
+      {/*Heading and description*/}
+      <div className="lg:w-1/2">
+        <h1 className="text-7xl font-extrabold pb-6 gradient-title">
+          Shop Your Way, Anytime
+        </h1>
+        <p className="text-xl text-gray-600 dark:text-gray-300 mb-10">
+          Discover the easiest way to shop for physical and virtual products.
+          Enjoy fast, secure payments with credit cards, Stripe, and more. Your
+          perfect purchase is just a click away!
+        </p>
+        <Link href="/dashboard">
+          <button className="flex items-center px-3 py-4 rounded-lg text-xl text-white bg-indigo-700 hover:bg-indigo-600">
+            Get Started <ArrowRight className="ml-2 w-5 h-5" />
+          </button>
+        </Link>
       </div>
-      <div className="col-span-1">right</div>
+
+      {/*Poster*/}
+      <div className="lg:w-1/2 flex justify-center">
+        <div className="relative w-full aspect-square max-w-md">
+          <Image
+            src="/poster.png"
+            alt="poster"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
     </div>
   );
 };
