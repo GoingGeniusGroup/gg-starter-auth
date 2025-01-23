49e6b4fdece81e878df45f0a5b1211c187bf4cb3
Aayush <ayushkarkee6@gmail.com>
2025-01-23 13:31:11 +0545

    update :: overlapped avatar in hero section

--- app/components/landingPage/HeroSection.tsx
+++ app/components/landingPage/HeroSection.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 const HeroSection = () => {
   return (
@@ -19,10 +20,29 @@ const HeroSection = () => {
           perfect purchase is just a click away!
         </p>
         <Link href="/dashboard">
-          <button className="flex items-center px-3 py-4 rounded-lg text-xl text-white bg-indigo-700 hover:bg-indigo-600">
+          <button className="flex items-center px-3 py-4 rounded-lg text-xl text-white bg-indigo-700 hover:bg-indigo-600 mb-8">
             Get Started <ArrowRight className="ml-2 w-5 h-5" />
           </button>
         </Link>
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col">
+            <div className="flex">
+              <Avatar className="-mr-4 z-10">
+                <AvatarImage src="https://i.pravatar.cc/150?img=1" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <Avatar className="-mr-4 z-20">
+                <AvatarImage src="https://i.pravatar.cc/150?img=2" />
+                <AvatarFallback>AB</AvatarFallback>
+              </Avatar>
+              <Avatar className="z-30">
+                <AvatarImage src="https://i.pravatar.cc/150?img=3" />
+                <AvatarFallback>XY</AvatarFallback>
+              </Avatar>
+            </div>
+          </div>
+          <div className="flex flex-col">hello</div>
+        </div>
       </div>
 
       {/*Poster*/}
