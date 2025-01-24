16f7f66f112697f7030359943fc0d985e3a7876f
rajarya199 <aryalrajan078@gmail.com>
2025-01-24 15:07:27 +0545

    update components

--- app/(main)/profile/[username]/page.tsx
+++ app/(main)/profile/[username]/page.tsx
@@ -42,32 +42,7 @@ const page = async() => {
       <div className='flex flex-col md:flex-row gap-8'>
       <div className="w-full md:w-1/3">
       <div className="w-full  flex  ">
-      {/* <div className="bg-white dark:bg-black rounded-lg shadow-lg p-6 w-full max-w-md">
-        <div className="flex flex-col items-center">
-          <img
-          src="https://www.sourcenepal.com/wp-content/uploads/2023/10/Hari-Bansha-Acharya-Age.webp"
-            alt="hari"
-            className="w-24 h-24 rounded-full object-cover border-2 border-gray-200"
-          />
-          <h1 className="mt-4 text-2xl font-semibold text-gray-800 dark:text-slate-100">
-            {user?.username}
-          </h1>
-          <div className="flex items-center mt-2 text-gray-600 dark:text-slate-50">
-            <Briefcase size={16} className="mr-2" />
-            <span>Computer Engineer</span>
-          </div>
-          <div className="w-full mt-6 space-y-4">
-            <div className="flex items-center text-gray-600  dark:text-slate-50">
-              <Mail size={16} className="mr-3" />
-              <span>{user?.email}</span>
-            </div>
-            <div className="flex items-center text-gray-600  dark:text-slate-50">
-              <Phone size={16} className="mr-3" />
-              <span>{user?.phone}</span>
-            </div>
-          </div>
-        </div>
-      </div> */}
+     
     {user && <ProfileCard userId={user.id}/>} 
     </div>
         </div>
