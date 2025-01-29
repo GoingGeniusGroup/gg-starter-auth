d9029e98cd99515bfbbc3a26323fdcb8d12838e7
rajarya199 <aryalrajan078@gmail.com>
2025-01-29 14:38:24 +0545

    loading ,loader

--- components/catComp/CategoryDetail.tsx
+++ components/catComp/CategoryDetail.tsx
@@ -3,14 +3,19 @@ import { getCategory } from '@/action/category';
 import React,{useState,useEffect} from 'react'
 import { Package } from "lucide-react";
 import { ImageSlider } from './ImageSlider';
+import BeatLoader from "react-spinners/BeatLoader"; 
+
 interface categoryProps{
     categoryId:string;
 }
 const CategoryDetail = ({categoryId}:categoryProps) => {
     const[categoryInfo,setCategoryInfo]=useState<any>(null)
+            const [loading, setLoading] = useState<boolean>(true)
+    
       useEffect(() => {
                 async function fetchData() {
-            
+                    setLoading(true)
+
                     try {
                         const response = await getCategory(categoryId);
                         if (response.success && response.data) {
@@ -22,11 +27,20 @@ const CategoryDetail = ({categoryId}:categoryProps) => {
                         }
                     } catch (error) {
                         console.error("Failed to fetch data", error);
+                    } finally{
+                        setLoading(false)
                     }
                     
                 }
                 fetchData();
             }, [categoryId]);
+            if (loading) {
+                return (
+                    <div className="flex justify-center items-center my-20 h-full">
+                             <BeatLoader color="#123abc" loading={loading} size={16} /> 
+                    </div>
+                );
+            }
         
   return (
     <div>
@@ -52,7 +66,7 @@ const CategoryDetail = ({categoryId}:categoryProps) => {
               </p>
               <div className="border-t pt-6">
                 <div className="flex items-center justify-between text-sm">
-                  <span className="text-gray-500">Available Products</span>
+                  <span className="text-gray-500">Products In This Category:</span>
                   <span className="font-medium text-gray-900">
                     {categoryInfo?.products?.length}
                   </span>
