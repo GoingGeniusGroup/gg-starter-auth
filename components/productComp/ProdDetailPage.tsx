bbcd8366a3bfae1078534385adaed60f87d4b21b
rajarya199 <aryalrajan078@gmail.com>
2025-01-31 11:31:52 +0545

    fetch data-prod

--- components/productComp/ProdDetailPage.tsx
+++ components/productComp/ProdDetailPage.tsx
@@ -1,6 +1,40 @@
-import React from 'react'
-
-const ProdDetailPage = () => {
+import React,{useState,useEffect} from 'react'
+import { getProductDetail } from '@/action/product';
+interface prodProps{
+  productId:string;
+}
+const ProdDetailPage = ({productId}:prodProps) => {
+  const [prodInfo,setProdInfo]=useState<any>(null)   
+    const [loading, setLoading] = useState<boolean>(true)
+    useEffect(() => {
+                  async function fetchData() {
+                      setLoading(true)
+  
+                      try {
+                          const response = await getProductDetail(productId);
+                          if (response.success && response.data) {
+                              console.log(response.data); 
+                          
+                              setProdInfo(response.data)
+                          } else {
+                              console.error("Failed to fetch  data");
+                          }
+                      } catch (error) {
+                          console.error("Failed to fetch data", error);
+                      } finally{
+                          setLoading(false)
+                      }
+                      
+                  }
+                  fetchData();
+              }, [productId]);
+              if(loading){
+                return(
+                  <div>
+                    loading data...
+                  </div>
+                )
+              }
   return (
     <div>ProdDetailPage</div>
   )
