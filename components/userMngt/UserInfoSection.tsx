2b296493eab3f2515cf1bd6127a3aa8e857af25f
rajarya199 <aryalrajan078@gmail.com>
2025-02-04 11:48:29 +0545

    user section

---
+++ components/userMngt/UserInfoSection.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { getAllUsers } from '@/action/user';
+import React,{useState,useEffect} from 'react'
+
+const UserInfoSection = () => {
+     const [userInfo, setUserInfo] = useState<any[]>([]);
+            const [loading, setLoading] = useState<boolean>(true)
+    useEffect(() => {
+        async function fetchData() {
+          setLoading(true)
+            try {
+                const response = await getAllUsers();
+                if (response.success && response.data) {
+                    console.log(response.data); 
+                    setUserInfo(response.data);
+                } else {
+                    console.error("Failed to fetch user data");
+                }
+            } catch (error) {
+                console.error("Failed to fetch data", error);
+            }
+            finally{
+              setLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
+  return (
+    <div>UserInfoSection</div>
+  )
+}
+
+export default UserInfoSection
