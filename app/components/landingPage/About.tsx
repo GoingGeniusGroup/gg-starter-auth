ed308c93f2d111ae459b23568a5c70059a046e26
Aayush <ayushkarkee6@gmail.com>
2025-02-05 13:16:12 +0545

    update :: completed about section

--- app/components/landingPage/About.tsx
+++ app/components/landingPage/About.tsx
@@ -1,3 +1,4 @@
+import { CircleDollarSign, HardDrive, Truck } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 
@@ -12,7 +13,37 @@ const About = () => {
           height={500}
         />
       </div>
-      <div className="lg:w-1/2">right</div>
+      <div className="lg:w-1/2">
+        <h1 className="text-4xl font-extrabold pb-6 gradient-title">
+          Transform your POS, Next-Level Experience
+        </h1>
+        <p className="text-xl text-gray-600 dark:text-gray-300 mb-10">
+          Transforming the way you conduct transcations, taking your
+          Point-of-Sale system to the next level means more than just an
+          upgradecustomer experiences, and unlocking new potentials
+        </p>
+
+        <div className="flex flex-col items-start justify-center gap-3">
+          <h2 className="flex items-center gap-2">
+            <div className="p-3 bg-slate-50 rounded-full">
+              <HardDrive className="text-slate-600" />
+            </div>
+            Hardware and POS System to sell anywhere{" "}
+          </h2>
+          <h2 className="flex items-center gap-2">
+            <div className="p-3 bg-yellow-50 rounded-full">
+              <Truck className="text-yellow-600" />
+            </div>
+            In-Store pickup, online ordering, delivery and shipment{" "}
+          </h2>
+          <h2 className="flex items-center gap-2">
+            <div className="p-3 bg-green-50 rounded-full">
+              <CircleDollarSign className="text-green-600" />
+            </div>
+            secure Payments anywhere your customers are{" "}
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
