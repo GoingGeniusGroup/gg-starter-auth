89010b29f49ad997f1cf8e4c57b17948c3c73300
rajarya199 <aryalrajan078@gmail.com>
2025-02-05 12:31:08 +0545

    pass user id

--- components/userMngt/UserTable.tsx
+++ components/userMngt/UserTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Table,
   TableBody,
@@ -12,48 +12,46 @@ import { Input } from "@/components/ui/input1";
 import { LuListFilter } from "react-icons/lu";
 import { BiShow } from "react-icons/bi";
 import { FaEdit, FaTrash, FaPlus } from "react-icons/fa";
-interface UserProps{
-  userInfo:any[]
+interface UserProps {
+  userInfo: any[];
 }
-const UserTable = ({userInfo}:UserProps) => {
+const UserTable = ({ userInfo }: UserProps) => {
   return (
     <div>
-        <Table className="w-full border-collapse border shadow rounded">
-          <TableHeader>
-            <TableRow>
-              <TableHead>S.N</TableHead>
-              <TableHead>UserName</TableHead>
-              <TableHead>FullName</TableHead>
-              <TableHead>Role</TableHead>
-              <TableHead>Balance</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>Phone</TableHead>
-              <TableHead>Address</TableHead>
-              <TableHead>Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {userInfo.map((user, index) => (
-              <TableRow key={user.id}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell className="text-blue-500">{user.userName}</TableCell>
-                <TableCell >{user.fullName ? user.fullName : 'N/A'}</TableCell>
-                <TableCell >{user.role}</TableCell>
-                <TableCell >{user.balance ? user.balance : "-"}</TableCell>
-                <TableCell >{user.email[0]}</TableCell>
-                <TableCell >{user.mobilePhone[0]}</TableCell>
-                <TableCell >{user.address[0]? user.address :"N/A"}</TableCell>
-                <TableCell>
-                  <div className="flex space-x-2">
-                    <button
-                      className="text-green-500 hover:text-green-700 text-xl"
-                      aria-label="View"
-                    >
-                      <BiShow />
-                    </button>
-                    <Link
-                      href={'/'}
-                    >
+      <Table className="w-full border-collapse border shadow rounded">
+        <TableHeader>
+          <TableRow>
+            <TableHead>S.N</TableHead>
+            <TableHead>UserName</TableHead>
+            <TableHead>FullName</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>Balance</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Phone</TableHead>
+            <TableHead>Address</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {userInfo.map((user, index) => (
+            <TableRow key={user.id}>
+              <TableCell>{index + 1}</TableCell>
+              <TableCell className="text-blue-500">{user.userName}</TableCell>
+              <TableCell>{user.fullName ? user.fullName : "N/A"}</TableCell>
+              <TableCell>{user.role}</TableCell>
+              <TableCell>{user.balance ? user.balance : "-"}</TableCell>
+              <TableCell>{user.email[0]}</TableCell>
+              <TableCell>{user.mobilePhone[0]}</TableCell>
+              <TableCell>{user.address[0] ? user.address : "N/A"}</TableCell>
+              <TableCell>
+                <div className="flex space-x-2">
+                  <button
+                    className="text-green-500 hover:text-green-700 text-xl"
+                    aria-label="View"
+                  >
+                    <BiShow />
+                  </button>
+                  <Link href={`/dashboard/account/update/${user.id}`}>
                       <button
                         className="text-blue-500 hover:text-blue-700 text-xl"
                         aria-label="Edit"
@@ -62,21 +60,20 @@ const UserTable = ({userInfo}:UserProps) => {
                       </button>
                     </Link>
 
-                    <button
-                    
-                      className="text-red-500 hover:text-red-700 text-xl"
-                      aria-label="Delete"
-                    >
-                      <FaTrash />
-                    </button>
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+                  <button
+                    className="text-red-500 hover:text-red-700 text-xl"
+                    aria-label="Delete"
+                  >
+                    <FaTrash />
+                  </button>
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
-export default UserTable
+export default UserTable;
