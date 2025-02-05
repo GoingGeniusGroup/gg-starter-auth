9cc1a9ad2e1ecf23f006662e4da27b5bf077008b
Aayush <ayushkarkee6@gmail.com>
2025-02-05 13:43:41 +0545

    update :: made new banner component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import HeroSection from "@/components/landingPage/HeroSection";
 import About from "../components/landingPage/About";
 import Service from "../components/landingPage/Service";
+import Banner from "../components/landingPage/Banner";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -13,6 +14,7 @@ export default async function Home() {
       <HeroSection />
       <About />
       <Service />
+      <Banner />
     </div>
   );
 }
---
+++ app/components/landingPage/Banner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="grid grid-cols-4">
+      <div className="col-span-3">left</div>
+      <div>right</div>
+    </div>
+  );
+};
+
+export default Banner;
