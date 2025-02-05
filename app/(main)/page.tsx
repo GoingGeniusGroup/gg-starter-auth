5a279ee5af7c220c30b2a8c9ef8772ea90c0ccb8
Aayush <ayushkarkee6@gmail.com>
2025-02-05 16:24:07 +0545

    update :: working on frequently asked questions

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,6 +4,7 @@ import About from "../components/landingPage/About";
 import Service from "../components/landingPage/Service";
 import Banner from "../components/landingPage/Banner";
 import Testimonials from "../components/landingPage/Testimonials";
+import Questions from "../components/landingPage/Questions";
 
 export const metadata: Metadata = {
   title: "Home",
@@ -17,11 +18,12 @@ export default async function Home() {
       <Service />
       <Banner />
       <div className="mb-24">
-        <h2 className="text-4xl font-semi-bold mb-12 gradient-title">
+        <h2 className="text-4xl font-semibold mb-12 gradient-title">
           What our users say
         </h2>
         <Testimonials />
       </div>
+      <Questions />
     </div>
   );
 }
---
+++ app/components/landingPage/Questions.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Questions = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="max-w-2xl mx-auto">
+        <h2 className="text-4xl font-semibold text-center pb-6 gradient-title">
+          Frequently asked questions
+        </h2>
+        <p className="text-lg text-center text-gray-600 dark:text-gray-300 mb-10">
+          We`ve complied a list of the most common questions we get asked. If
+          you have any other questions, please feel free to reach out to us
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
