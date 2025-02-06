0a1599ce76e8bb6717cd7544d52de0f802b7db54
rajarya199 <aryalrajan078@gmail.com>
2025-02-06 13:08:48 +0545

    add children,slot

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,12 @@
 "use client";
-
+import { usePathname } from "next/navigation";
 import Sidebar from "@/_components/dashboard/Sidebar";
 import Topbar from "@/_components/dashboard/Topbar";
 import { ReactNode, useState } from "react";
 
-const DashboardLayout = ({ children }: { children: ReactNode }) => {
+const DashboardLayout = ({ children,orderStat }: { children: ReactNode,orderStat:ReactNode }) => {
   const [isCollapsed, setIsCollapsed] = useState(false); // Sidebar state
+  const pathname = usePathname();
 
   return (
     <div className="flex flex-col h-screen relative py-4">
@@ -22,8 +23,14 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
           style={{
             marginLeft: isCollapsed ? "7rem" : "16rem",
           }}
-        >
+        > 
           {children}
+          {pathname === "/dashboard" && (
+        <div className="grid grid-cols-2 gap-4 mt-4">
+          <div>{orderStat}</div>
+        
+        </div>
+      )}
         </main>
       </div>
     </div>
