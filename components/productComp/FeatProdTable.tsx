7884b23f02eca1854689bde958e6ee3ccf744a6b
rajarya199 <aryalrajan078@gmail.com>
2025-02-06 13:15:00 +0545

    feat-prod-

---
+++ components/productComp/FeatProdTable.tsx
@@ -0,0 +1,20 @@
+"use client"
+import React,{useState,useEffect} from 'react'
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table1";
+  import { getAllproducts } from '@/action/product';
+  import Image from 'next/image';
+const FeatProdTable = () => {
+  return (
+    <div>FeatProdTable</div>
+  )
+}
+
+export default FeatProdTable
