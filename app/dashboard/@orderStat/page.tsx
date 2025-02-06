86e3343f76f5e7f479a360d59e649b14e35a36d9
rajarya199 <aryalrajan078@gmail.com>
2025-02-06 13:09:16 +0545

    order section

---
+++ app/dashboard/@orderStat/page.tsx
@@ -0,0 +1,8 @@
+import VirtOrderTable from "@/components/inventoryComponent/VirtOrderTable";
+import OrderStatTable from "@/components/orderComp/OrderStatTable";
+
+export default function OrderStat() {
+    return <div className="p-4">
+      <OrderStatTable/>
+    </div>;
+  }
