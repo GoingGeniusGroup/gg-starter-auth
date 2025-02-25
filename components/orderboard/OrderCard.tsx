c400db75e7f7bb81c09d814da92e85661206029c
rajarya199 <aryalrajan078@gmail.com>
2025-02-25 11:05:50 +0545

    order-card

--- components/orderboard/OrderCard.tsx
+++ components/orderboard/OrderCard.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Draggable } from "@hello-pangea/dnd";
 import { format } from "date-fns";
+import Image from "next/image";
 interface orderProp{
     index:number,
     order:any
 }
+import { User, Package, } from "lucide-react";
+
 export function OrderCard({
   order,
   index
@@ -17,9 +20,20 @@ export function OrderCard({
               {format(new Date(order.date), "MMM d, yyyy")}
             </span>
           </div>
-          <div className="text-sm mb-2">{order.customer}</div>
-          <div className="text-sm text-gray-600 mb-2">
-            {order.products.join(", ")}
+          <div className="flex items-center text-sm mb-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              className="w-4 h-4 text-blue-600 mr-2"
+              fill="currentColor"
+            >
+              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
+            </svg>
+            <span>{order.customer}</span>
+          </div>
+          <div className="flex items-center text-sm text-gray-600 mb-2">
+            <Package size={18} className="text-orange-600 mr-2" />
+            <span>{order.products.join(", ")}</span>
           </div>
           <div className="text-right text-green-500 font-medium">
             ${order.total.toFixed(2)}
