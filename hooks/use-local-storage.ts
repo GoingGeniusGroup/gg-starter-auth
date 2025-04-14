004be8ab78fa6f625cbba4a03e837c0f5b43bc69
Aayush <ayushkarkee6@gmail.com>
2025-04-14 21:41:45 +0545

    creating local storage hooks

---
+++ hooks/use-local-storage.ts
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T) {
+  // State to store our value
+  const [storedValue, setStoredValue] = useState<T>(initialValue);
+
+  // Initialize the state
+  useEffect(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      setStoredValue(item ? JSON.parse(item) : initialValue);
+    } catch (error) {
+      console.error(error);
+      setStoredValue(initialValue);
+    }
+  }, [key, initialValue]);
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+
+      // Save to local storage
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue] as const;
+}
